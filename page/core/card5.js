b561e371c240f71a425f92e9ec063df0e89eb452
klvnhdy <kelpin145@gmail.com>
2024-03-18 13:48:18 +0700

    page core

--- page/core/card5.js
+++ page/core/card5.js
@@ -1,4 +1,39 @@
 const core1card5 = a => {
 	a = el({a:'div', b:a, d:{id:'core1card5'} })
-	
+	el({a:'div', b:a, c:'EBR Not Achieves'});
+
+	(a => {
+		const lines2 = el({a:'div', b:a})
+		var options = {
+			series: [{
+			name: 'series1',
+			data: [31, 40, 28, 51, 42, 109, 100]
+		  		}],
+			chart: {
+			height: 150,
+			type: 'area'
+		  },
+		  dataLabels: {
+			enabled: false
+		  },
+		  stroke: {
+			curve: 'smooth'
+		  },
+		  xaxis: {
+			type: 'datetime',
+			categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+		  },
+		  tooltip: {
+			x: {
+			  format: 'dd/MM/yy HH:mm'
+			},
+		  },
+		  };
+  
+		  var chart = new ApexCharts(lines2, options);
+		  chart.render();
+
+
+	})(el({a:'div', b:a, d:{} }))
+
 }
