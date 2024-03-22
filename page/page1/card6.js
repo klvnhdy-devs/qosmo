5705a4a553452012a718c138281c6925f958f4be
Miftahul Munir <miftahulmunir001@gmail.com>
2024-03-22 11:07:53 +0700

    gamas per treg

--- page/page1/card6.js
+++ page/page1/card6.js
@@ -1,38 +1,47 @@
 const page1card6 = a => {
 	a = el({a:'div', b:a, d:{id:'page1card6'} })
 
-	el({a:'div', b:a, c:'Gangguan Impact To Quality', d:{style:'font-size:2.5vmin; font-weight:bold; margin-bottom:1vh;'} });
+	el({a:'div', b:a, c:'Gamas per Treg', d:{style:'font-size:2.5vmin; font-weight:bold; margin-bottom:1vh;'} });
 
 	(a => {
 		const gangguan = el({a:'div', b:a})
-		var options = {
-			series: [{
-			data: [400, 430, 448]
-		  }],
+		const options = {
+			series: [{name:'', data: [0, 0, 0, 0, 0, 0, 0]}],
 			chart: {
-			toolbar: { show: false },
-			type: 'bar',
-			height: 200
-		  },
-		  plotOptions: {
-			bar: {
-			  borderRadius: 4,
-			  horizontal: true,
-			  distributed: true,
+				toolbar: { show: false },
+				type: 'bar',
+				height: 250
+			},
+			plotOptions: {
+				bar: {
+					borderRadius: 3,
+					//horizontal: true,
+					//distributed: true,
+				}
+			},
+			dataLabels: {
+				enabled: false
+			},
+			colors: ['#5788FE', ],
+			xaxis: {
+				categories: [],
 			}
-		  },
-		  dataLabels: {
-			enabled: false
-		  },
-		  colors: ['#FBC246', '#5788FE', '#FD2D6C'],
-		  xaxis: {
-			categories: ['Core', 'Telkomsel', 'Access',],
-		  }
-		  };
-  
-		  var chart = new ApexCharts(gangguan, options);
-		  chart.render();
+		};
 
+		m.p1.card6 = new ApexCharts(gangguan, options)
+		m.p1.card6.render()
+
+		fetch('api.php?cmd=total-gamas-treg').then(a=>a.json()).then(a => {
+			m.p1.card6.updateOptions({
+				series: [{
+					name: 'gamas',
+					data: a.data.map(a=>parseInt(a.site_not_clear))
+				}],
+				xaxis: {
+					categories: a.data.map(a=>a.region)
+				}
+			})
+		})
 
 	})(el({a:'div', b:a, d:{} }))
 
